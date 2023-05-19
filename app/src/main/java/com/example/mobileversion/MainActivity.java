16c4cb127ac0c7440cd074421dfc572277b94564
Aylin-ai <ajlinkadan3224@gmail.com>
2023-05-19 22:14:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mobileversion/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mobileversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import models.LoginFragment;
+import models.RegistrationFragment;
+import models.TabFragment;
+import models.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LoginFragment(), "Авторизация");
+        adapter.addFragment(new RegistrationFragment(), "Регистрация");
+        viewPager.setAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/models/RegistrationFragment.java
@@ -0,0 +1,98 @@
+package models;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.mobileversion.MainActivity;
+import com.example.mobileversion.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class RegistrationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+        // Дополнительная логика для фрагмента регистрации
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = "jdbc:mysql://127.0.0.1:3306/antrap";
+        String username = "root";
+        String password = "";
+
+        EditText userPassword1 = view.findViewById(R.id.userPassword1);
+        EditText userPassword2 = view.findViewById(R.id.userPassword2);
+        EditText userEmail = view.findViewById(R.id.userEmail);
+
+
+
+        Button registrationButton = view.findViewById(R.id.registrationButton);
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Password1 = userPassword1.getText().toString();
+                String Password2 = userPassword2.getText().toString();
+                String Email = userEmail.getText().toString();
+                if (Password1.equals("") ||
+                Password2.equals("") || Email.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Ошибка")
+                            .setMessage("Введены не все значения");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return;
+                }
+                if (!Password1.equals(Password2)){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Ошибка")
+                            .setMessage("Пароли не совпадают");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return;
+                }
+
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.createUserWithEmailAndPassword(Email, Password1)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getContext(), "Authentication successful.",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(getContext(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
+            }
+        });
+
+        return view;
+    }
+}
+
